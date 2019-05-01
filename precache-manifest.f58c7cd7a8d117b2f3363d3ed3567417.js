c0df4ea7a652ad6bf79e3a5122d30db7db74f8d3
Андрей Романов <romanov_andrey_vl@protonmail.com>
2019-05-01 18:28:02 +0300

    Updates

---
+++ precache-manifest.f58c7cd7a8d117b2f3363d3ed3567417.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3b0ad3efe76353809fe3c52dfa31b58",
+    "url": "/reactify-final/index.html"
+  },
+  {
+    "revision": "aa1e16e34370025236d6",
+    "url": "/reactify-final/static/css/2.cf0c22ac.chunk.css"
+  },
+  {
+    "revision": "5c6860661cb0051c9c73",
+    "url": "/reactify-final/static/css/main.b54d288b.chunk.css"
+  },
+  {
+    "revision": "aa1e16e34370025236d6",
+    "url": "/reactify-final/static/js/2.b202a9a7.chunk.js"
+  },
+  {
+    "revision": "5c6860661cb0051c9c73",
+    "url": "/reactify-final/static/js/main.7f00fed8.chunk.js"
+  },
+  {
+    "revision": "e08938700afd3d91a28e",
+    "url": "/reactify-final/static/js/runtime~main.d0891a28.js"
+  }
+]);
